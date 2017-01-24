d84c81ac42582f94595f43e5a09835e2ba2710ca
Anthony Robin <contact@anthony-robin.fr>
2017-01-24 16:09:03 +0100

    Setup Blog sidebar displaying Categories

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,6 @@
 class BlogsController < ApplicationController
   before_action :set_blog, only: [:show, :edit, :update, :destroy]
+  before_action :set_categories, only: [:index, :show]
 
   # GET /blogs
   # GET /blogs.json
@@ -65,6 +66,10 @@ class BlogsController < ApplicationController
   # Use callbacks to share common setup or constraints between actions.
   def set_blog
     @blog = Blog.friendly.find(params[:id])
+  end
+
+  def set_categories
+    @categories = Category.all
   end
 
   # Never trust parameters from the scary internet, only allow the white list through.
