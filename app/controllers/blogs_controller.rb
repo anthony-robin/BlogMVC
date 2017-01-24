b6942a1da523b7dcd0e34d95b16aff992b584742
Anthony Robin <contact@anthony-robin.fr>
2017-01-24 15:44:20 +0100

    Add Category select field to Blog form

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -69,6 +69,6 @@ class BlogsController < ApplicationController
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def blog_params
-    params.require(:blog).permit(:title, :slug, :content)
+    params.require(:blog).permit(:title, :slug, :content, :category_id)
   end
 end
