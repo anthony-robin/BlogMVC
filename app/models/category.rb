dbe6a2416b65e7a4c4c342ec83285310adf57ac2
Anthony Robin <contact@anthony-robin.fr>
2017-01-24 15:29:52 +0100

    Add FriendlyId to Category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,10 @@
 class Category < ApplicationRecord
+  extend FriendlyId
+  friendly_id :name, use: :slugged
+
   has_many :blogs
+
+  def should_generate_new_friendly_id?
+    slug.blank? || new_record? || name_changed?
+  end
 end
