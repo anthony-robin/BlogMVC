c03ddb0bf9d65a66df66445ae9a9c0f1bf4ac6aa
Anthony Robin <contact@anthony-robin.fr>
2017-02-03 18:54:12 +0100

    Setup FriendlyId integration for User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,13 @@
 class User < ApplicationRecord
+  extend FriendlyId
+  friendly_id :username, use: :slugged
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def should_generate_new_friendly_id?
+    new_record?
+  end
 end
