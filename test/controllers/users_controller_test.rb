f956c589c821a1b4af7488f12c44235ebfa12036
Anthony Robin <contact@anthony-robin.fr>
2017-02-04 12:12:58 +0100

    Fix UsersController test issue

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,8 +1,12 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get users_index_url
+  setup do
+    sign_in users(:one)
+  end
+
+  test 'should get index' do
+    get users_url
     assert_response :success
   end
 end
