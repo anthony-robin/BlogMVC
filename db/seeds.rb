fb22cf8a570f05dab50406c13b21d43550e9b3bd
Anthony Robin <contact@anthony-robin.fr>
2017-02-05 15:14:52 +0100

    Add second user in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,6 +11,13 @@ user = User.create!(
   password_confirmation: 'password'
 )
 
+user2 = User.create!(
+  username: 'Nimda Nimda',
+  email: 'nimda@example.com',
+  password: 'password',
+  password_confirmation: 'password'
+)
+
 puts 'Create blogs categories'
 10.times do
   Category.create(
@@ -24,6 +31,6 @@ puts 'Create blogs articles'
     title: Faker::Hipster.words(4).join(' '),
     content: "<p>#{Faker::Hipster.paragraph}</p>",
     category_id: Category.all.map(&:id).sample,
-    user_id: user.id
+    user_id: [user.id, user2.id].sample
   )
 end
