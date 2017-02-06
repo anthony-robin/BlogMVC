cb98a133f08a506710a68f982fb3c9d338af5ea8
Anthony Robin <contact@anthony-robin.fr>
2017-02-06 18:31:25 +0100

    Add missing breadcrumbs in blogs and categories views

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -9,14 +9,46 @@ crumb :blogs do
   link t('blogs.index.title'), blogs_path
 end
 
-crumb :category do |category|
+crumb :blog_category do |category|
   link category.name, category_blogs_path(category)
   parent :blogs
 end
 
 crumb :blog do |blog|
   link blog.title, category_blog_path(blog.category, blog)
-  parent blog.category
+  parent :blog_category, blog.category
+end
+
+crumb :new_blog do
+  link t('blogs.new.title'), new_blog_path
+  parent :blogs
+end
+
+crumb :edit_blog do |blog|
+  link t('blogs.edit.title'), edit_category_blog_path(blog.category, blog)
+  parent :blog_category, blog.category
+end
+
+# -----------
+# Category
+#------------
+crumb :categories do
+  link t('categories.index.title'), categories_path
+end
+
+crumb :category do |category|
+  link category.name, category_path(category)
+  parent :categories
+end
+
+crumb :new_category do
+  link t('categories.new.title'), new_category_path
+  parent :categories
+end
+
+crumb :edit_category do |category|
+  link t('categories.edit.title'), edit_category_path(category)
+  parent :categories
 end
 
 # -----------
