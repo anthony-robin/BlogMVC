e5aa7d8caad4a11f9def63a723fe853c3126b6a5
Anthony Robin <contact@anthony-robin.fr>
2017-02-18 23:25:30 +0100

    Exclude spec/ folder from CodeClimate analysis, remove unnecessary support files

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -10,6 +10,11 @@ ActiveRecord::Migration.maintain_test_schema!
 Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
 RSpec.configure do |config|
+  config.extend ControllerMacros, type: :controller
+  config.include FactoryGirl::Syntax::Methods
+  config.include AbstractController::Translation
+  config.include Devise::Test::ControllerHelpers, type: :controller
+
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
