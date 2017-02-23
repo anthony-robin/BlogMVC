8c2ae8089e4738123c84184c75fb245c618c1757
Anthony Robin <contact@anthony-robin.fr>
2017-02-23 21:11:12 +0100

    Remove unnecessary 'bullet: true'

--- spec/controllers/homes_controller_spec.rb
+++ spec/controllers/homes_controller_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe HomesController, bullet: true do
+describe HomesController do
   describe 'GET #index' do
     before(:each) { get :index }
     it_behaves_like :ok_request, 'index'
