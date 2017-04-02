55f1d496aef34ad5df673b3338b5e54ac842f4e8
Anthony Robin <contact@anthony-robin.fr>
2017-04-02 11:39:16 +0200

    SETUP dotenv gem

--- config/initializers/sentry_raven.rb
+++ config/initializers/sentry_raven.rb
@@ -1,6 +1,6 @@
 if Rails.env.staging? || Rails.env.production?
   Raven.configure do |config|
-    config.dsn = ENV['SENTRY_URL']
+    config.dsn = ENV.fetch('SENTRY_URL')
     config.environments = %w(staging production)
   end
 end
