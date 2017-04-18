793dafbacfdb565f32175524f0ab724dce277568
Anthony Robin <contact@anthony-robin.fr>
2017-04-18 20:35:36 +0200

    SETUP .eslintrc file, FIX offenses

--- app/assets/javascripts/cable.js
+++ app/assets/javascripts/cable.js
@@ -1,13 +1,10 @@
 // Action Cable provides the framework to deal with WebSockets in Rails.
 // You can generate new channels where WebSocket features live using the rails generate channel command.
-//
-//= require action_cable
-//= require_self
-//= require_tree ./channels
+
+import ActionCable from 'action_cable'
+import App from 'app'
 
 (function() {
-  this.App || (this.App = {});
-
-  App.cable = ActionCable.createConsumer();
-
-}).call(this);
+  this.App || (this.App = {})
+  App.cable = ActionCable.createConsumer()
+}).call(this)
