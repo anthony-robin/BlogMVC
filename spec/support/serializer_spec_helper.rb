5dfecfd8cda5b61ddb305730db44e8c8a8457466
Anthony Robin <contact@anthony-robin.fr>
2017-05-04 23:00:51 +0200

    FIX Rubocop offenses

--- spec/support/serializer_spec_helper.rb
+++ spec/support/serializer_spec_helper.rb
@@ -1,5 +1,5 @@
 module SerializerSpecHelper
-  def serialize(obj, opts={})
+  def serialize(obj, opts = {})
     serializer_class = opts.delete(:serializer_class) || "#{obj.class.name}Serializer".constantize
     serializer = serializer_class.send(:new, obj)
     adapter = ActiveModelSerializers::Adapter.create(serializer, opts)
