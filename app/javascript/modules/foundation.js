18ec0d31e042cee51e6a324f78679c26decaa45c
Anthony Robin <contact@anthony-robin.fr>
2017-06-24 11:05:14 +0200

    MIGRATE to Foundation 6.4.0-rc5
    `Foundation` 6.4 is still in release candidate but I chose
    to start using it in order to prepare the transition
    to the new `grid` system.
    
    **Details**
    - UPDATE `Foundation` to latest release candidate
    - REPLACE old `flex` grid by new `xy-grid` one
    - CREATE partial for `flash` markup
    - FIX broken `breadcrumbs` markup
    - USE short `slim` syntax

--- app/javascript/modules/foundation.js
+++ app/javascript/modules/foundation.js
@@ -4,15 +4,21 @@ import 'motion-ui/dist/motion-ui.css'
 
 // Javascript
 import 'foundation.core'
-import 'foundation.dropdownMenu'
-import 'foundation.orbit'
+
+import 'foundation.util.mediaQuery'
 import 'foundation.util.keyboard'
 import 'foundation.util.box'
 import 'foundation.util.nest'
 import 'foundation.util.motion'
 import 'foundation.util.touch'
-import 'foundation.util.mediaQuery'
-import 'foundation.util.timerAndImageLoader'
+import 'foundation.util.timer'
+import 'foundation.util.triggers'
+import 'foundation.util.imageLoader'
+
+import 'foundation.dropdownMenu'
+import 'foundation.orbit'
+import 'foundation.responsiveToggle'
+
 import 'motion-ui/dist/motion-ui'
 
 document.addEventListener('turbolinks:load', () => {
