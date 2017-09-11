2b451b5a8757908066790dcd7db22a5c6c3cf58b
Anthony Robin <contact@anthony-robin.fr>
2017-09-11 22:13:21 +0200

    ADD --only-failure option to rspec
    Details:
    - ADD `--only-failure` option to rspec
    - ADD `spec/examples.txt` to `.gitignore`

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,6 +14,7 @@ RSpec.configure do |config|
   config.default_formatter = 'doc' if config.files_to_run.one?
   config.profile_examples = 10
   config.order = :random
+  config.example_status_persistence_file_path = 'spec/examples.txt'
 
   Kernel.srand config.seed
 end
