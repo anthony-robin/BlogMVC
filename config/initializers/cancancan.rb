fb5ebc899f3a5e7be6491a3ccd28716e7239da97
Anthony Robin <contact@anthony-robin.fr>
2017-10-01 10:36:10 +0200

    MAKE cancancan and friendly_id happy
    Details:
    - UPDATE actual fix with a shorter code

--- config/initializers/cancancan.rb
+++ config/initializers/cancancan.rb
@@ -1,22 +1,11 @@
-if defined?(CanCan)
-  class Object
-    def metaclass
-      class << self; self; end
-    end
-  end
-
-  module CanCan
-    module ModelAdapters
-      class ActiveRecord4Adapter < AbstractAdapter
-        @@friendly_support = {}
-
-        def self.find(model_class, id)
-          klass =
-          model_class.metaclass.ancestors.include?(ActiveRecord::Associations::CollectionProxy) ?
-            model_class.klass : model_class
-          @@friendly_support[klass] ||= klass.metaclass.ancestors.include?(FriendlyId)
-          @@friendly_support[klass] == true ? model_class.friendly.find(id) : model_class.find(id)
-        end
+# Hotfix that makes Cancancan to play nicely with FriendlyId
+# @example https://github.com/CanCanCommunity/cancancan/issues/106
+module CanCan
+  module ModelAdapters
+    class AbstractAdapter
+      def self.find(model_class, id)
+        model_class.respond_to?(:friendly) ?
+          model_class.friendly.find(id) : model_class.find(id)
       end
     end
   end
